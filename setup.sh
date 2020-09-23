bc827f1a04c3ca3d9615c86dd0fdaeab131f9a27
Rixt <rixtdieuwke@live.nl>
2020-09-23 13:44:23 +0200

    nginx works. also with ssl

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
 echo "This script will setup all your applications."
+minikube delete
+minikube start --driver=virtualbox
+# enable necessary addons
+minikube addons enable metallb
+minikube addons enable dashboard
+# on first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/metallb.yaml
+
+
+# set environment variables (type env to check if they are added)
+eval $(minikube docker-env)
+# make nginx image
+docker build -t mynginx ./srcs/nginx/
+# deployment nginx
+kubectl apply -f srcs/nginx/nginx.yaml
