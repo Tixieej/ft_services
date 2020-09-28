632f4c486645c84534cf65e62a9645f8816642c2
Rixt <rixtdieuwke@live.nl>
2020-09-28 14:16:02 +0200

    ftps part is done

--- setup.sh
+++ setup.sh
@@ -8,13 +8,19 @@ minikube addons enable dashboard
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/metallb.yaml
 
-
 # set environment variables (type env to check if they are added)
-eval $(minikube docker-env)
+eval $(minikube -p minikube docker-env)
 # make nginx image
 docker build -t mynginx ./srcs/nginx/
 # deployment nginx
 kubectl apply -f srcs/nginx/nginx.yaml
 
-# ftps on port 21
-kubectl apply -f srcs/ftps/ftps-alpine.yaml
+# make ftps image
+docker build -t my_ftps ./srcs/ftps/
+# deployment ftps
+kubectl apply -f srcs/ftps/ftps.yaml
+
+# make wordpress image
+docker build -t my_wp ./srcs/wordpress/
+# deployment wordpress
+kubectl apply -f srcs/wordpress/wp.yaml
