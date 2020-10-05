cbef5b257409ababa7c8035659602f52c963a9a3
Rixt <rixtdieuwke@live.nl>
2020-10-05 14:21:52 +0200

    wordpress works, users can be added

--- setup.sh
+++ setup.sh
@@ -10,26 +10,32 @@ kubectl apply -f srcs/metallb.yaml
 
 # set environment variables (type env to check if they are added)
 eval $(minikube -p minikube docker-env)
+
+echo "\033[92mNginx\033[0m"
 # make nginx image
 docker build -t mynginx ./srcs/nginx/
 # deployment nginx
 kubectl apply -f srcs/nginx/nginx.yaml
 
+echo "\033[92mftps\033[0m"
 # make ftps image
 docker build -t my_ftps ./srcs/ftps/
 # deployment ftps
 kubectl apply -f srcs/ftps/ftps.yaml
 
+echo "\033[92mMySQL\033[0m"
 # make mysql image
 docker build -t my_mysql ./srcs/mysql/
 # deployment mysql
 kubectl apply -f srcs/mysql/mysql.yaml
 
+echo "\033[92mphpMyAdmin\033[0m"
 # make phpmyadmin image
 docker build -t my_phpmyadmin ./srcs/php/
 # deployment phpmyadmin
 kubectl apply -f srcs/php/php.yaml
 
+echo "\033[92mWordpress\033[0m"
 # make wordpress image
 docker build -t my_wp ./srcs/wordpress/
 # deployment wordpress
---
+++ srcs/wordpress/startup.sh
@@ -0,0 +1,17 @@
+php-fpm7
+nginx
+cd www
+wp db create
+
+APISERVER=https://kubernetes.default.svc
+SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
+TOKEN=$(cat ${SERVICEACCOUNT}/token)
+CACERT=${SERVICEACCOUNT}/ca.crt
+URL=$(curl --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api/v1/namespaces/default/services/wordpress/ 2>/dev/null| jq -r '.status | .loadBalancer | .ingress | .[] | .ip')
+
+wp core install --title=Wordpress --admin_user=rixt --admin_password=secret --admin_email=rde-vrie@student.codam.nl --skip-email --url=${URL}:5050
+
+wp user create remco rpet@student.codam.nl --user_pass=kaaskip --url=${URL}:5050
+
+
+wp user create remco rpet@student.codam.nl --user_pass=kaaskip --url=${URL}:5050
