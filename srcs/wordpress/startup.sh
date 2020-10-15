52a34476b71498da0dc2372a706f554052ca5719
Tixieej <rixtdieuwke@live.nl>
2020-10-15 22:37:40 +0200

    cool nginx homepage and fixed wp ip

--- srcs/wordpress/startup.sh
+++ srcs/wordpress/startup.sh
@@ -8,7 +8,7 @@ TOKEN=$(cat ${SERVICEACCOUNT}/token)
 CACERT=${SERVICEACCOUNT}/ca.crt
 URL=$(curl --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api/v1/namespaces/default/services/wordpress/ 2>/dev/null | jq -r '.status | .loadBalancer | .ingress | .[] | .ip')
 
-wp core install --title=Wordpress --admin_user=rixt --admin_password=secret --admin_email=rde-vrie@student.codam.nl --skip-email --url=192.168.99.223:5050
+wp core install --title=Wordpress --admin_user=rixt --admin_password=secret --admin_email=rde-vrie@student.codam.nl --skip-email --url=192.168.99.220:5050
 
 wp user create remco rpet@student.codam.nl --user_pass=kaaskip 
 
